a60d084966b4455463ad44d1e99ee5640d1ac447
alan-fiig <alan.figueroa@hotmail.com>
2024-08-23 12:35:26 -0600

    Add db users

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_10_172934) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_23_171944) do
   create_table "tasks", force: :cascade do |t|
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
